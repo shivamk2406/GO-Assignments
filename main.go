2bf21285ee2d7dffc465d4e6e9ee00e3cbb27f7d
Blaezy <iamrealshivamskyler@gmail.com>
2022-04-04 15:23:08 +0530

    Cleared Main

--- main.go
+++ main.go
@@ -6,32 +6,5 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
-	var choice int
-
-	for choice != 5 {
-		fmt.Println("1.  Add User details.")
-		fmt.Println("2.  Display User details.")
-		fmt.Println("3.  Delete User details")
-		fmt.Println("4.  Save User details.")
-		fmt.Println("5.  Exit")
-		fmt.Println("Enter Your Choice:")
-		fmt.Scan(&choice)
-
-		switch choice {
-		case 1:
-			fmt.Println("Added User")
-		case 2:
-			fmt.Println("Display User")
-		case 3:
-			fmt.Println("Delete User")
-		case 4:
-			fmt.Println("Save User Details")
-		case 5:
-			fmt.Println("Exit")
-		default:
-			fmt.Println("Invalid Input!!! Please enter a valid choice")
-		}
-
-	}
 
 }
