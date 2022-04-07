00bb4dc3383053fd4af5005f04861351657b3ef5
shivamk2406 <iamrealshivamskyler@gmail.com>
2022-04-07 11:17:48 +0530

    seperated constants

--- view/view.go
+++ view/view.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/pkg/errors"
 	itemdetails "github.com/shivamk2406/GO-Assignments/item"
 	"github.com/shivamk2406/GO-Assignments/item/enum"
 )
@@ -20,7 +19,6 @@ func Initialize() error {
 	fmt.Printf("Name: %s\nPrice: %f \nQuantity: %d \nType: %s \nTax: %f \nEffective Price: %f\n", itemInvoice.Name, itemInvoice.Price, item.Quantity, itemInvoice.Type, itemInvoice.Tax, itemInvoice.EffectivePrice)
 
 	moreItems, err := getUserChoice()
-
 	for err != nil {
 		moreItems, err = getUserChoice()
 	}
@@ -32,50 +30,46 @@ func Initialize() error {
 	return nil
 }
 
-func getItem() (item itemdetails.Item, err error) {
+func getItem() (itemdetails.Item, error) {
 	var name string
 	var price float64
 	var quantity int
 	var itemType string
 
 	fmt.Println("Enter Item Name")
-	_, err = fmt.Scanf("%s", &name)
+	_, err := fmt.Scanf("%s", &name)
 	if err != nil {
-		err = errors.Wrap(err, "item name scanning failed")
 		log.Println(err)
-		return
+		return itemdetails.Item{}, fmt.Errorf("item name scanning failed")
 	}
 
 	fmt.Println("Enter Item Price")
 	_, err = fmt.Scanf("%f", &price)
 	if err != nil {
-		err = errors.Wrap(err, "item name scanning failed")
 		log.Println(err)
-		return
+		return itemdetails.Item{}, fmt.Errorf("item price scanning failed")
 	}
 
 	fmt.Println("Enter Item Quantity")
 	_, err = fmt.Scanf("%d", &quantity)
 	if err != nil {
-		err = errors.Wrap(err, "item quantity scanning failed")
 		log.Println(err)
-		return
+		return itemdetails.Item{}, fmt.Errorf("item quantity scanning failed")
 	}
 
 	fmt.Println("Enter Item Type")
 	_, err = fmt.Scanf("%s", &itemType)
 	if err != nil {
-		err = errors.Wrap(err, "item type scanning failed")
 		log.Println(err)
-		return
+		return itemdetails.Item{}, fmt.Errorf("item quantity scanning failed")
 	}
 
-	item, err = itemdetails.NewItem(name, price, quantity, itemType)
+	item, err := itemdetails.NewItem(name, price, quantity, itemType)
 	if err != nil {
-		return
+		return itemdetails.Item{}, err
 	}
 
-	return
+	return item, nil
 }
 
 func getUserChoice() (string, error) {
@@ -83,14 +77,13 @@ func getUserChoice() (string, error) {
 	userResponse := enum.Accept
 	_, err := fmt.Scanf("%s", &userResponse)
 	if err != nil {
-		err = errors.Wrap(err, "Scan for user choice failed")
 		log.Println(err)
-		return userResponse, err
+		return userResponse, fmt.Errorf("scan for user chhoice failed")
 	}
 
 	if err := validateUserResponse(userResponse); err != nil {
-		err = errors.Wrap(err, "invalid user response")
-		return userResponse, err
+
+		return userResponse, fmt.Errorf("invalid user response")
 	}
 
 	return userResponse, nil
