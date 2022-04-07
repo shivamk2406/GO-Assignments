c7c4cf6c7a1605675f4f184565d885df909e398f
shivamk2406 <iamrealshivamskyler@gmail.com>
2022-04-07 11:29:26 +0530

    added all constants

---
+++ config/constants.go
@@ -0,0 +1,16 @@
+package config
+
+const (
+	Accept                          = "y"
+	Deny                            = "n"
+	rawItemTaxRate                  = 0.125
+	baseManufacturedItemTaxRate     = 0.125
+	addedManufacturedItemTaxRate    = 0.02
+	rateForAddedManufacturedItemTax = 112.5
+	importedItemTaxRate             = 0.1
+	limit1Surcharge                 = 5
+	limit2Surcharge                 = 10
+	limit3SurchargeRate             = 0.05
+	surchargeLimit1                 = 100
+	surchargeLimit2                 = 200
+)
