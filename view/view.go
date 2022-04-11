47247aa43c6125e4a7fcb96c499a4dfcce6e3dec
shivamk2406 <iamrealshivamskyler@gmail.com>
2022-04-11 17:22:31 +0530

    added top two functionalities

--- view/view.go
+++ view/view.go
@@ -2,21 +2,75 @@ package view
 
 import (
 	"fmt"
+	"log"
 	"os"
 
-	user "github.com/shivamk2406/GO-Assignments/tree/Assignment-2/user"
+	"github.com/pkg/errors"
+	"github.com/shivamk2406/GO-Assignments/tree/Assignment-2/aggregate"
+	"github.com/shivamk2406/GO-Assignments/tree/Assignment-2/services"
 )
 
-func DriverMenu() {
+func driveMenu() {
+	fmt.Println("1.  Add User details.")
+	fmt.Println("2.  Display User details.")
+	fmt.Println("3.  Delete User details")
+	fmt.Println("4.  Save User details.")
+	fmt.Println("5.  Exit")
+
+}
+
+func Initialize() error {
+	choice := 0
+	tempStudents := []aggregate.Student{}
+
+	for choice != 5 {
+		driveMenu()
+		choice, err := getChoice()
+		if err != nil {
+			return err
+		}
+		switch choice {
+		case 1:
+			tempStudent, err := services.GetStudentDetails()
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+			tempStudents = append(tempStudents, tempStudent)
+			fmt.Println("Student added successfully")
+		case 2:
+			services.DisplayStudentDetails(tempStudents)
+		case 5:
+			os.Exit(1)
+		}
+	}
+	return nil
+}
+
+func getChoice() (int, error) {
+	var choice int
+	fmt.Println("Enter Your Choice: ")
+	_, err := fmt.Scanf("%d", &choice)
+	if err != nil {
+		log.Println(err)
+		return 0, errors.Errorf("choice scanning failed")
+	}
+
+	return choice, nil
+
+}
+
+func validateUserChoice(choice int) error {
+	if choice >= 1 && choice <= 5 {
+		return errors.Errorf("invalid choice")
+	}
+	return nil
+}
+
+/*func DriverMenu() {
 	var choice int
 	TempUser := user.User{}
 	for choice != 5 {
-
-		fmt.Println("1.  Add User details.")
-		fmt.Println("2.  Display User details.")
-		fmt.Println("3.  Delete User details")
-		fmt.Println("4.  Save User details.")
-		fmt.Println("5.  Exit")
 
 		fmt.Println("Enter Your Choice:")
 		fmt.Scan(&choice)
@@ -45,4 +99,4 @@ func DriverMenu() {
 		}
 
 	}
-}
+}*/
