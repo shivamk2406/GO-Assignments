d780576765ddcf490a500ef01f98b0ce8529683d
shivamk2406 <iamrealshivamskyler@gmail.com>
2022-04-28 12:45:56 +0530

    refactored

--- item-inventory/internal/service/service.go
+++ item-inventory/internal/service/service.go
@@ -2,14 +2,12 @@ package service
 
 import (
 	"fmt"
-	"math/rand"
 	"sync"
 	"time"
 
 	"github.com/shivamk2406/item-inventory/internal/config"
 	"github.com/shivamk2406/item-inventory/internal/service/consumer"
 	"github.com/shivamk2406/item-inventory/internal/service/item"
-	"github.com/shivamk2406/item-inventory/internal/service/item/enum"
 	producer "github.com/shivamk2406/item-inventory/internal/service/producer"
 	"github.com/shivamk2406/item-inventory/pkg/database"
 )
@@ -59,40 +57,4 @@ func Util(repo item.DB) {
 
 	wg.Wait()
 	fmt.Printf("Total Length of invoice generated %d \n", len(invoices))
-}
-
-func BatchInsertion(repo item.DB) {
-	rand.Seed(time.Now().UnixNano())
-	var items []item.Item
-	names := [6]string{
-		"pen",
-		"paper",
-		"Book",
-		"Copy",
-		"Pencil",
-		"Eraser",
-	}
-
-	types := [3]string{
-		"manufactured",
-		"imported",
-		"raw",
-	}
-
-	for i := 0; i < 10000; i++ {
-		types, _ := enum.ItemTypeString(types[rand.Intn(2)+1])
-		item := item.Item{
-			Name:     names[rand.Intn(5)+1],
-			Price:    100 + rand.Float64()*100,
-			Quantity: rand.Intn(8) + 2,
-			Type:     types,
-		}
-		fmt.Println(item)
-		items = append(items, item)
-	}
-
-	repo.BatchInsertion(items)
-
-	fmt.Println(len(items))
-
 }
---
+++ item-inventory/tests/tests.go
@@ -0,0 +1,46 @@
+package e2e
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/shivamk2406/item-inventory/internal/service/item"
+	"github.com/shivamk2406/item-inventory/internal/service/item/enum"
+)
+
+func BatchInsertion(repo item.DB) {
+	rand.Seed(time.Now().UnixNano())
+	var items []item.Item
+	names := [6]string{
+		"pen",
+		"paper",
+		"Book",
+		"Copy",
+		"Pencil",
+		"Eraser",
+	}
+
+	types := [3]string{
+		"manufactured",
+		"imported",
+		"raw",
+	}
+
+	for i := 0; i < 10000; i++ {
+		types, _ := enum.ItemTypeString(types[rand.Intn(2)+1])
+		item := item.Item{
+			Name:     names[rand.Intn(5)+1],
+			Price:    100 + rand.Float64()*100,
+			Quantity: rand.Intn(8) + 2,
+			Type:     types,
+		}
+		fmt.Println(item)
+		items = append(items, item)
+	}
+
+	repo.BatchInsertion(items)
+
+	fmt.Println(len(items))
+
+}
